24b70c019e653268e4a380e0f04eb53f79c4acf1
Fabian Padilla <padillacastanedacesarfabian@gmail.com>
2025-01-27 21:15:48 -0600

    primer commit, proyecto avanzado hasta el video 3

---
+++ app/Controllers/Crud.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class Crud extends BaseController
+{
+    public function index(): string
+    {
+        return view('listado');
+    }
+
+
+    public function crear (){
+        
+    }
+
+    public function actualizar(){
+
+    }
+
+    public function obtenerNombre (){
+
+    }
+
+
+}
